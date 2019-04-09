eb348ddddd8f546abc7239ebe94ad39d4ab5b590
scottinet <seb.cottinet@gmail.com>
2019-04-09 17:43:13 +0200

    Add missing XML documentation

--- Kuzzle/API/Controllers/BaseController.cs
+++ Kuzzle/API/Controllers/BaseController.cs
@@ -3,6 +3,9 @@
   /// Base controller.
   /// </summary>
   public class BaseController {
+    /// <summary>
+    /// Kuzzle instance.
+    /// </summary>
     protected readonly Kuzzle kuzzle;
 
     internal BaseController(Kuzzle k) {
