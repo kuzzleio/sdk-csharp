f1f5efc651e59fa41950865c7c8948f2ff5df306
scottinet <seb.cottinet@gmail.com>
2019-06-06 14:23:53 +0200

    [ci] don't use relative paths

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-nuget pack Kuzzle/Kuzzle.nuspec -OutputFileNamesWithoutVersion
+nuget pack $TRAVIS_BUILD_DIR/Kuzzle/Kuzzle.nuspec -OutputFileNamesWithoutVersion -OutputDirectory $TRAVIS_BUILD_DIR
 
 nuget push kuzzlesdk.nupkg -ApiKey ${NUGET_API_KEY} -Source nuget.org
