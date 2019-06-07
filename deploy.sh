320c01a990665011411ced1f8c0d01dbec279064
scottinet <seb.cottinet@gmail.com>
2019-06-07 10:31:53 +0200

    [ci] will I ever be able to install nuget 5?

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,14 @@ set -ex
 NUSPEC_FILE="${TRAVIS_BUILD_DIR}/Kuzzle/Kuzzle.nuspec"
 NUPKG_FILE="${TRAVIS_BUILD_DIR}/kuzzlesdk.nupkg"
 
-# current distributions have nuget v4 only but we need at least a v5 to
+# Current distributions have nuget v4 only but we need at least a v5 to
 # match the latest nuspec specifications
+# But first we have some configuring to do to allow nuget to update itself
+# through SSL
+sudo mozroots --import --machine --sync
+sudo certmgr -ssl -m https://go.microsoft.com
+sudo certmgr -ssl -m https://nugetgallery.blob.core.windows.net
+sudo certmgr -ssl -m https://nuget.org
 nuget update -self
 
 nuget pack ${NUSPEC_FILE} -OutputDirectory ${TRAVIS_BUILD_DIR} -OutputFileNamesWithoutVersion
