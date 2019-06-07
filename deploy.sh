d55b852a36d446e5c5150f9ed50c6cfc259e21fa
scottinet <seb.cottinet@gmail.com>
2019-06-07 11:21:50 +0200

    [ci] workaround to nuget user collision bug

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,10 @@ yes | sudo certmgr -ssl -m https://nugetgallery.blob.core.windows.net
 yes | sudo certmgr -ssl -m https://nuget.org
 sudo nuget update -self
 
+# And now, we need to clear the NuGet cache created by the update command
+# above (see https://github.com/NuGet/Home/issues/6537)
+sudo chmod -R 777 /tmp/NuGetScratch/*
+
 nuget pack ${NUSPEC_FILE} -OutputDirectory ${TRAVIS_BUILD_DIR} -OutputFileNamesWithoutVersion
 
 nuget push ${NUPKG_FILE} -ApiKey ${NUGET_API_KEY} -Source nuget.org
