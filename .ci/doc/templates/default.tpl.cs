b77bb3810784122061beef3b83f25cd1aea571db
Aschen <amaret93@gmail.com>
2019-06-25 12:01:49 +0200

    wip

---
+++ .ci/doc/templates/default.tpl.cs
@@ -0,0 +1,12 @@
+#r "Kuzzle.dll"
+using System;
+// using KuzzleSdk;
+// using KuzzleSdk.API;
+// using KuzzleSdk.Protocol;
+// using System.Threading.Tasks;
+
+// KuzzleSdk.Kuzzle kuzzle = new KuzzleSdk.Kuzzle(new WebSocket("kuzzle"));
+
+// await kuzzle.ConnectAsync();
+
+[snippet-code]
