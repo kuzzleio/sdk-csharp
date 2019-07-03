b6d5662e525d41e97837667ea5e9a3435bd4c179
ycombes <ycombes@kuzzle.io>
2019-07-03 10:44:02 +0200

    add AdminController methods [dump, loadFixtures, loadMappings, loadSecurities, resetCache, resetDatabase, resetKuzzleData, resetSecurity, shutdown] and unit tests for method [dump]

---
+++ Kuzzle.Tests/API/Controllers/AdminControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using KuzzleSdk.API.Controllers;
+using Newtonsoft.Json.Linq;
+using KuzzleSdk.Exceptions;
+using Moq;
+
+namespace Kuzzle.Tests.API.Controllers {
+  public class AdminControllerTest {
+    private readonly AdminController _adminController;
+    private readonly KuzzleApiMock _api;
+
+    public AdminControllerTest() {
+      _api = new KuzzleApiMock();
+      _adminController = new AdminController(_api.MockedObject);
+    }
+
+
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public async void DumpAsyncTestSuccess(bool value) {
+
+      _api.SetResult(new JObject {
+          {"result", new JObject {{"acknowledge", value}}
+        }
+      });
+
+      bool result = await _adminController.DumpAsync();
+
+      _api.Verify(new JObject {
+        {"controller", "admin"},
+        {"action", "dump"}
+      });
+
+      Assert.Equal(value, result);
+    }
+  }
+}
---
+++ Kuzzle/API/Controllers/AdminController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace KuzzleSdk.API.Controllers {
+  public class AdminController : BaseController {
+    internal AdminController(IKuzzleApi k) : base(k) { }
+
+
+    /// <summary>
+    /// Asynchronously create a snapshot of Kuzzle's state.
+    /// Depending on the configuration of Kuzzle.
+    /// </summary>
+    public async Task<bool> DumpAsync() {
+
+      Response response = await api.QueryAsync(new JObject {
+        {"controller", "admin"},
+        {"action", "dump"}
+      });
+
+      return (bool)response.Result["acknowledge"];
+    }
+
+    /// <summary>
+    /// Load fixtures into the storage layer.
+    /// </summary>
+    public async Task<bool> LoadFixturesAsync(JObject indexName, string refresh = null) {
+
+      Response response = await api.QueryAsync(new JObject {
+        {"controller", "admin"},
+        {"action", "loadFixtures"},
+        {"refresh", refresh},
+        {"body", new JObject {
+            {"index-name", indexName}
+          }
+        }
+      });
+
+      return (bool)response.Result["acknowledge"];
+    }
+
+    /// <summary>
+    /// Apply mappings to the storage layer.
+    /// </summary>
+    public async Task<bool> LoadMappingsAsync(JObject indexName, string refresh = null) {
+
+      Response response = await api.QueryAsync(new JObject {
+        {"controller", "admin"},
+        {"action", "loadMappings"},
+        {"refresh", refresh},
+        {"body", new JObject {
+            {"index-name", indexName}
+          }
+        }
+      });
+
+      return (bool)response.Result["acknowledge"];
+    }
+
+    /// <summary>
+    /// Load roles, profiles and users into the storage layer.
+    /// </summary>
+    public async Task<bool> LoadSecuritiesAsync(JObject body, string refresh = null) {
+
+      Response response = await api.QueryAsync(new JObject {
+        {"controller", "admin"},
+        {"action", "loadSecurities"},
+        {"refresh", refresh},
+        {"body", body}
+      });
+
+      return (bool)response.Result["acknowledge"];
+    }
+
+    /// <summary>
+    /// Asynchronously clears the cache database.
+    /// </summary>
+    public async Task<bool> ResetCacheAsync(string database) {
+
+      Response response = await api.QueryAsync(new JObject {
+        {"controller", "admin"},
+        {"action", "resetCache"},
+        {"database", database}
+      });
+
+      return (bool)response.Result["acknowledge"];
+    }
+
+    /// <summary>
+    /// Asynchronously deletes all indexes created by users.
+    /// Neither Kuzzle internal indexes nor Plugin indexes are deleted.
+    /// </summary>
+    public async Task<bool> ResetDatabaseAsync(string database) {
+
+      Response response = await api.QueryAsync(new JObject {
+        {"controller", "admin"},
+        {"action", "resetDatabase"},
+      });
+
+      return (bool)response.Result["acknowledge"];
+    }
+
+    /// <summary>
+    /// Asynchronously starts the following sequence, in this order:
+    /// - Invalidates and deletes all users along with their associated credentials
+    /// - Deletes all user-defined roles and profiles
+    /// - Resets the default roles and profiles to their default values
+    //Deletes all document validation specifications
+    //
+    //This action has no impact on Plugin and Document storages.
+    /// </summary>
+    public async Task<bool> ResetKuzzleDataAsync() {
+
+      Response response = await api.QueryAsync(new JObject {
+        {"controller", "admin"},
+        {"action", "resetKuzzleData"},
+      });
+
+      return (bool)response.Result["acknowledge"];
+    }
+
+    /// <summary>
+    /// Asynchronously deletes all users, profiles and roles.
+    /// Then resets anonymous, default and admin profiles and roles
+    /// to default values, specified in Kuzzle configuration files.
+    /// </summary>
+    public async Task<bool> ResetSecurityAsync() {
+
+      Response response = await api.QueryAsync(new JObject {
+        {"controller", "admin"},
+        {"action", "resetSecurity"},
+      });
+
+      return (bool)response.Result["acknowledge"];
+    }
+
+    /// <summary>
+    /// Safely stops a Kuzzle instance after all remaining requests are processed.
+    ///
+    /// In a cluster environment, the shutdown action will be propagated across all nodes.
+    /// </summary>
+    public async Task<bool> ShutdownAsync() {
+
+      Response response = await api.QueryAsync(new JObject {
+        {"controller", "admin"},
+        {"action", "shutdown"},
+      });
+
+      return (bool)response.Result["acknowledge"];
+    }
+
+  }
+}
