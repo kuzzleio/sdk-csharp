61c87312d23a3b56af949c327d01d99bc35732be
ycombes <ycombes@kuzzle.io>
2019-07-03 14:07:13 +0200

    add unit tests for [loadFixtures, loadMappings]

--- Kuzzle.Tests/API/Controllers/AdminControllerTest.cs
+++ Kuzzle.Tests/API/Controllers/AdminControllerTest.cs
@@ -34,5 +34,70 @@ namespace Kuzzle.Tests.API.Controllers {
 
       Assert.Equal(value, result);
     }
+
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public async void LoadFixturesAsyncTestSuccess(bool value) {
+
+      _api.SetResult(new JObject {
+          {"result", new JObject {{"acknowledge", value}}
+        }
+      });
+
+      bool result = await _adminController.LoadFixturesAsync(
+        JObject.Parse(@"{foo: [{create: {_id: 'bar'}}, {field: 'value', field2: true}]}"),
+        "wait_for");
+
+      _api.Verify(new JObject {
+        {"controller", "admin"},
+        {"action", "loadFixtures"},
+        {"refresh", "wait_for"},
+        {"body", new JObject {
+          {"index-name", new JObject{
+            {"foo", new JArray {
+              new JObject{{"create", new JObject {{ "_id", "bar" }} } },
+              new JObject{{"field", "value"}, {"field2", true} }
+            }}
+          }}
+        }}
+      });
+        
+      Assert.Equal(value, result);
+    }
+
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public async void LoadMappingsAsyncTestSuccess(bool value) {
+
+      _api.SetResult(new JObject {
+          {"result", new JObject {{"acknowledge", value}}
+        }
+      });
+
+      bool result = await _adminController.LoadFixturesAsync(
+        JObject.Parse(@"{foo: {properties: {field1: {}, field2: {}}}}"),
+        "wait_for");
+
+      _api.Verify(new JObject {
+        {"controller", "admin"},
+        {"action", "loadFixtures"},
+        {"refresh", "wait_for"},
+        {"body", new JObject {
+          {"index-name", new JObject{
+            {"foo", new JObject {
+              {"properties", new JObject{
+                {"field1", new JObject{ }},
+                {"field2", new JObject{ }}
+              }}
+            }}
+          }}
+        }}
+      });
+
+      Assert.Equal(value, result);
+    }
+
   }
 }
