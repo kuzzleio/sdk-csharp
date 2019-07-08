609b94b5f836fa61fe8eb6e73697fe3b70c6d2a1
ycombes <ycombes@kuzzle.io>
2019-07-08 17:32:09 +0200

    modify the program to check subscription and logout

--- Kuzzle.Client/Program.cs
+++ Kuzzle.Client/Program.cs
@@ -3,21 +3,34 @@ using System.Threading;
 using System.Threading.Tasks;
 using KuzzleSdk;
 using KuzzleSdk.Protocol;
+using Newtonsoft.Json.Linq;
 
 namespace Kuzzle.Client {
   class Program {
     static void Main(string[] args) {
       WebSocket socket = new WebSocket(new Uri("ws://localhost:7512"));
+      socket.AutoReconnect = true;
       KuzzleSdk.Kuzzle kuzzle = new KuzzleSdk.Kuzzle(socket);
 
       kuzzle.ConnectAsync(CancellationToken.None).Wait();
 
-
       Task.Run(async () => {
+        await kuzzle.Realtime.SubscribeAsync("foo", "bar", new JObject { }, null);
+        int i = 0;
         while (true) {
           Task.Run(async () => {
-            long x = await kuzzle.Server.NowAsync();
-            Console.WriteLine(x);
+            if (kuzzle.NetworkProtocol.State == ProtocolState.Reconnecting) {
+              if (i % 5 == 0) {
+                await kuzzle.Auth.LogoutAsync();
+              } else {
+                long x = await kuzzle.Server.NowAsync();
+                Console.WriteLine(x);
+              }
+            } else {
+              long x = await kuzzle.Server.NowAsync();
+              Console.WriteLine(x);
+            }
+            i = i + 1;
           });
           await Task.Delay(500);
         }
