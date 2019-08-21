8fc0983de1ee40c21a06eda2c78fcffc848d6d09
scottinet <seb.cottinet@gmail.com>
2019-08-21 14:32:46 +0200

    [documentation] oops..

--- doc/1/controllers/document/m-delete/snippets/m-delete.cs
+++ doc/1/controllers/document/m-delete/snippets/m-delete.cs
@@ -5,7 +5,7 @@ try {
     "yellow-taxi",
     new string[] { "some-id", "some-other-id" });
 
-  Console.WriteLine($"Successfully deleted {deleted.Count} documents");
+  Console.WriteLine($"Successfully deleted {deleted.Length} documents");
 } catch (KuzzleException e) {
   Console.Error.WriteLine(e);
 }
